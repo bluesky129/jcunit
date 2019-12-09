0dadc42e4afd7dac501935df867bb455816af8ab
hiroshi.ukai <dakusui@gmail.com>
2019-12-09 16:03:00 +0900

    Add preliminary study note

--- src/test/java/com/github/dakusui/jcunit8/experiments/join/acts/ActsUtilsTest.java
+++ src/test/java/com/github/dakusui/jcunit8/experiments/join/acts/ActsUtilsTest.java
@@ -11,11 +11,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-import static com.github.dakusui.crest.Crest.allOf;
-import static com.github.dakusui.crest.Crest.asInteger;
-import static com.github.dakusui.crest.Crest.asString;
-import static com.github.dakusui.crest.Crest.assertThat;
-import static com.github.dakusui.crest.Crest.call;
+import static com.github.dakusui.crest.Crest.*;
 import static com.github.dakusui.jcunit8.extras.generators.Acts.readTestSuiteFromCsv;
 
 public class ActsUtilsTest {
@@ -49,6 +45,199 @@ public class ActsUtilsTest {
     generateAndReportWithConstraints(baseDir, 20, 3);
     generateAndReportWithConstraints(baseDir, 30, 3);
     generateAndReportWithConstraints(baseDir, 40, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsStrength1Factors10() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 10, 1);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsStrength1Factors20() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 20, 1);
+  }
+  @Test
+  public void testGenerateAndReportWithConstraintsStrength1Factors30() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 30, 1);
+  }
+  @Test
+  public void testGenerateAndReportWithConstraintsStrength2Factors10() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 10, 2);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsStrength2Factors20() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 20, 2);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsStrength2Factors30() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 30, 2);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factors10() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 10, 3);
+  }
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factors20() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 20, 3);
+  }
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factors30() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 30, 3);
+  }
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factors40() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 40, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factors50() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 50, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor60() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 60, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor70() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 70, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor80() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 80, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor90() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 90, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor100() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 100, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor110() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 110, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor120() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 120, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor130() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 130, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor140() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 140, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor150() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 150, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor160() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 160, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor170() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 170, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor180() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 180, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor190() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 190, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength3Factor200() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 200, 3);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength4Factor10() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 10, 4);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength4Factor20() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 20, 4);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength4Factor30() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 30, 4);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength5Factor10() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 10, 5);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength5Factor20() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 20, 5);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength6Factor10() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 10, 6);
+  }
+
+  @Test
+  public void testGenerateAndReportWithConstraintsWithStrength6Factor20() {
+    File baseDir = UTUtils.createTempDirectory("target/acts");
+    generateAndReportWithConstraints(baseDir, 20, 6);
   }
 
   @SuppressWarnings("unchecked")
