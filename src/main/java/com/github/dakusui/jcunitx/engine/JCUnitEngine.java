effbd6f4a97d5a65f6052a61883c469cfd151dea
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2021-02-10 11:58:00 +0900

    Configure pom.xml to use JUnit4 for unit testing.

--- src/main/java/com/github/dakusui/jcunitx/engine/JCUnitEngine.java
+++ src/main/java/com/github/dakusui/jcunitx/engine/JCUnitEngine.java
@@ -2,9 +2,6 @@ package com.github.dakusui.jcunitx.engine;
 
 import org.junit.platform.engine.*;
 
-import java.util.Optional;
-import java.util.Set;
-
 public class JCUnitEngine implements TestEngine {
   @Override
   public String getId() {
@@ -13,67 +10,7 @@ public class JCUnitEngine implements TestEngine {
 
   @Override
   public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
-    return new TestDescriptor() {
-      @Override
-      public UniqueId getUniqueId() {
-        return null;
-      }
-
-      @Override
-      public String getDisplayName() {
-        return null;
-      }
-
-      @Override
-      public Set<TestTag> getTags() {
-        return null;
-      }
-
-      @Override
-      public Optional<TestSource> getSource() {
-        return Optional.empty();
-      }
-
-      @Override
-      public Optional<TestDescriptor> getParent() {
-        return Optional.empty();
-      }
-
-      @Override
-      public void setParent(TestDescriptor parent) {
-
-      }
-
-      @Override
-      public Set<? extends TestDescriptor> getChildren() {
-        return null;
-      }
-
-      @Override
-      public void addChild(TestDescriptor descriptor) {
-
-      }
-
-      @Override
-      public void removeChild(TestDescriptor descriptor) {
-
-      }
-
-      @Override
-      public void removeFromHierarchy() {
-
-      }
-
-      @Override
-      public Type getType() {
-        return null;
-      }
-
-      @Override
-      public Optional<? extends TestDescriptor> findByUniqueId(UniqueId uniqueId) {
-        return Optional.empty();
-      }
-    };
+    return (new JCUnitDiscoverer()).discover(discoveryRequest, uniqueId);
   }
 
   @Override
